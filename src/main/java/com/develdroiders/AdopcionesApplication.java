f861f79d2a12b0b00ebafbe1922fd690b35055ed
Deuda Técnica <develdroiders@gmail.com>
2016-10-23 19:41:38 +0200

    first commit with controller, security and templates

---
+++ src/main/java/com/develdroiders/AdopcionesApplication.java
@@ -0,0 +1,12 @@
+package com.develdroiders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionesApplication.class, args);
+	}
+}
